5e9b73c15c3619adbc8ddb023d7adc079988b9a8
gulinodesign-maker <gulinodesign@gmail.com>
2025-11-28 09:44:50 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "music-rainbow-v2-iphone-landscape";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+  // Puoi aggiungere qui altri file statici (css/js/font) se vuoi che vadano in cache offline.
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return (
+        cached ||
+        fetch(event.request).catch(() =>
+          // Se offline e non in cache, non facciamo fallback speciali.
+          cached
+        )
+      );
+    })
+  );
+});
